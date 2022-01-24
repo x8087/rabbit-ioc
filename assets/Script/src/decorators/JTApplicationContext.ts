6f044856d1cedd318c39d62c6cd55125a80ee3ce
SamWey <lovewechatmaru@gmail.com>
2022-01-24 22:38:34 +0800

    1.6.1
    修复BUG
    export  default class JTUserInfo extends JTAbstractUserInfo
    {
            public groupInfos:any[] = null;
            public dialogChats:any[] = null;
            public friendInfos:JTFriendInfo[] = null;
            constructor()
            {
                    super();
                    this.friendInfos = [];
            }
    
            @c.Qualifier("__userInfo")
            @c.Bean
            public userInfo():JTUserInfo
            {
                return new JTUserInfo();
            }
    }
    export default class JTUserInfoManager extends com.JTDataInfoManager
    {
        @c.Qualifier("__userInfo")
        @c.Autowired
        public userInfo:JTUserInfo = null;
        constructor()
        {
            super();
        }
    }

--- assets/Script/src/decorators/JTApplicationContext.ts
+++ assets/Script/src/decorators/JTApplicationContext.ts
@@ -4,43 +4,42 @@ namespace com
 {
     export class JTApplicationContext
     {
-        public static ____contextMap:Object = {};
+        public static ___contextMap:Object = {};
         public static ___contexts:JTClassContext[] = [];
 
-        protected getObjectByClass(___class:any):any
+        protected getObjectByClass(___class:any):JTClassContext
         {
-
+            return null;
         }
 
         public static collect(caller:any, property:string, descripter:any):void
         {
-            let __cxt:JTClassContext = this.____contextMap[property];
-            if (__cxt)
+            let __context:JTClassContext = this.___contextMap[property];
+            if (__context)
             {
                 error("already inject " + property  + " bean object. need change other property name!");
             }
-            this.____contextMap[property] = __cxt = new JTClassContext();
-            __cxt.bind(caller, property, descripter);
+            this.___contextMap[property] = __context = new JTClassContext();
+            __context.bind(caller, property, descripter);
         }
 
         public static getContext(property:string):JTClassContext
         {
-            let __changedProperty:string = JTPropertyMapping.getProperty(property);
-            return this.____contextMap[__changedProperty];
+            return this.___contextMap[property];
         }
 
         public static changedPropertyName(caller:any, __sourceProperty:string, descripter:any, __changedProperty:string):void
         {
             let  ___c:JTClassContext = null;
-            for (let key in this.____contextMap)
+            for (let key in this.___contextMap)
             {
-                ___c = this.____contextMap[key];
+                ___c = this.___contextMap[key];
                 if (___c.___descripter !== descripter) continue;
                 ___c.__changedProperty = __changedProperty;
-                this.____contextMap[__changedProperty] = ___c;
+                this.___contextMap[__changedProperty] = ___c;
                 
-                delete this.____contextMap[__sourceProperty];
-                this.____contextMap[__sourceProperty] = null;
+                delete this.___contextMap[__sourceProperty];
+                this.___contextMap[__sourceProperty] = null;
                 break;
             }
         }
@@ -64,29 +63,58 @@ namespace com
            return;
         }
         JTApplicationContext.collect(caller, property, descripter);
-        JTPropertyMapping.registerPropertyMapping(property);
     };
 
     export var Qualifier:Function = function(changedProperty:string)
     {
-        return function(caller:any, sourceProperty:string, descripter:any)
+        return function(___caller:any, __property:string, descripter:any)
         {
             if (!descripter) //属性获取时，先修改对属性获取BEAN对象的属性名;
             {
-               
+                Object.defineProperty(___caller, __property, {});
+                doAutowired(___caller, __property, changedProperty);
+                // let __descriptor:any = Object.getOwnPropertyDescriptor(___caller, __property)
+                // if (__descriptor) return;//避免重复注册钩子
+                // let key:string = JTDecoratorUtils.registerDecoratorKey(__property);
+                // Object.defineProperty(___caller, __property, 
+                // {
+                //     get: function () 
+                //     {
+                //         let val = this[key];
+                //         if (val === null || val === undefined) 
+                //         {
+                //             let ____c:JTClassContext = JTApplicationContext.getContext(changedProperty);
+                //             val = this[__property] = ____c.instance;
+                //             ____c = null;
+                //         }
+                //         return val;
+                //     },
+                //     set: function (val) 
+                //     {
+                //         let oldVal:any = this[key];
+                //         if (val === oldVal) return;
+                //         this[key] = val;
+                //     },
+                // enumerable: true,
+                // configurable: true
+                // });
             }
             else //方法或者类
             {
-                JTApplicationContext.changedPropertyName(caller, sourceProperty, descripter, changedProperty);
-                JTPropertyMapping.changedPropertyMapping(sourceProperty, changedProperty);
+                JTApplicationContext.changedPropertyName(___caller, __property, descripter, changedProperty);
             }
         }
     }
 
     export var Autowired:Function = function (___caller:any, __property:string)
     {
-        let __descriptor:any = Object.getOwnPropertyDescriptor(___caller, __property)
-        if (__descriptor) return;//避免重复注册钩子
+        doAutowired(___caller, __property, __property)
+    }
+    
+    function doAutowired(___caller:any, __property:string, changedProperty:string)
+    {
+        // let __descriptor:any = Object.getOwnPropertyDescriptor(___caller, __property)
+        // if (__descriptor) return;//避免重复注册钩子
         let key:string = JTDecoratorUtils.registerDecoratorKey(__property);
 		Object.defineProperty(___caller, __property, 
 		{
@@ -95,7 +123,7 @@ namespace com
 				let val = this[key];
 				if (val === null || val === undefined) 
 				{
-					let ____c:JTClassContext = JTApplicationContext.getContext( __property);
+					let ____c:JTClassContext = JTApplicationContext.getContext( changedProperty);
 					val = this[__property] = ____c.instance;
 					____c = null;
 				}
